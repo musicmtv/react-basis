03785098b8ef41f2b63b8c7a2172c569a13091b6
James Zhu <13205798@qq.com>
2018-03-11 10:53:39 +0800

    4

---
+++ 4/src/js/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import News from "./../components/News"
+
+
+
+
+
+ReactDOM.render(
+    <News newsid="101"/>,
+    document.getElementById('root')
+);
